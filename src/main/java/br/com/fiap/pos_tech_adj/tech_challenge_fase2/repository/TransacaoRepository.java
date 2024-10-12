687d53b3e00e7d0aa151253eb364fda917397299
Igor Guilherme <ifgandrade@outlook.com>
2024-10-12 10:53:13 -0300

    Corrigindo erro no agendador para retornar a ultima transacao para um determinada vaga

--- src/main/java/br/com/fiap/pos_tech_adj/tech_challenge_fase2/repository/TransacaoRepository.java
+++ src/main/java/br/com/fiap/pos_tech_adj/tech_challenge_fase2/repository/TransacaoRepository.java
@@ -1,9 +1,14 @@
 package br.com.fiap.pos_tech_adj.tech_challenge_fase2.repository;
 
 import br.com.fiap.pos_tech_adj.tech_challenge_fase2.model.Transacao;
+
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TransacaoRepository extends MongoRepository<Transacao, String> {
 
     Transacao findByVaga(String id);
+
+    Optional<Transacao> findFirstByVagaOrderByDataHoraDesc(String id);
 }
--- src/main/java/br/com/fiap/pos_tech_adj/tech_challenge_fase2/scheduled/TransacaoScheduled.java
+++ src/main/java/br/com/fiap/pos_tech_adj/tech_challenge_fase2/scheduled/TransacaoScheduled.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.util.List;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import br.com.fiap.pos_tech_adj.tech_challenge_fase2.model.Transacao;
 import br.com.fiap.pos_tech_adj.tech_challenge_fase2.model.Vaga;
@@ -22,20 +23,29 @@ public class TransacaoScheduled {
         this.transacaoRepository = transacaoRepository;
         this.vagaRepository = vagaRepository;
     }
-    
-    @Scheduled(fixedRate = 60000) // Executa a cada 1 minuto (60000 milissegundos)
-    public void verificarExpiracao(){
-        System.out.println("Verificando transações expiradas para desocupar vagas...");
 
-        List<Vaga> vagasOcupadas = vagaRepository.findByOcupada(true);
-        
-        for (Vaga vaga : vagasOcupadas) {
-           Transacao transacao = transacaoRepository.findByVaga(vaga.getId());
-           if (transacao != null && transacao.getDataExpiracao().isBefore(LocalDateTime.now())) {
-                vaga.setOcupada(false);
-                vagaRepository.save(vaga);
-                System.out.println("Vaga " + vaga.getId() + " desocupada.");
-           }
-        }       
+    @Scheduled(fixedRate = 60000) // Executa a cada 1 minuto (60000 milissegundos)
+    public void verificarExpiracao() {
+        try {
+            System.out.println("Verificando transações expiradas para desocupar vagas...");
+
+            List<Vaga> vagasOcupadas = vagaRepository.findByOcupada(true);
+
+            for (Vaga vaga : vagasOcupadas) {
+                Optional<Transacao> optionalTransacao = transacaoRepository
+                        .findFirstByVagaOrderByDataHoraDesc(vaga.getId());
+
+                if (optionalTransacao.isPresent()) {
+                    Transacao transacao = optionalTransacao.get();
+                    if (transacao != null && transacao.getDataExpiracao().isBefore(LocalDateTime.now())) {
+                        vaga.setOcupada(false);
+                        vagaRepository.save(vaga);
+                        System.out.println("Vaga " + vaga.getId() + " desocupada.");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao verificar transações expiradas: " + e.getMessage());
+        }
     }
 }
